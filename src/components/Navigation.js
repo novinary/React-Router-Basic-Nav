f92e119a0cc26388efe8a9e293d7224e352c31c1
novinary <novina.dev@gmail.com>
2019-03-04 13:49:56 +0000

    Declare to as the href on <Link> and specify correct routes for app to navigate towards

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,18 +3,18 @@ import { Link } from "react-router-dom";
 
 const Navigation = () => {
   return (
-    <div>
-      <div className="App">
-        <h1>React Router Mini</h1>
-        <div>
-          <a href="">Home</a>
-        </div>
-        <div>
-          <a href="">About</a>
-        </div>
-        <div>
-          <a href="">Contact</a>
-        </div>
+    <div className="App">
+      <h1>React Router Mini</h1>
+      <div>
+        <Link to={"/"}>Home</Link>
+      </div>
+      <div>
+        <Link to={"/About"} href="">
+          About
+        </Link>
+      </div>
+      <div>
+        <Link to={"/Contact"}>Contact</Link>
       </div>
     </div>
   );
